35db03c938f8911ac170db4d3bb805831565a0a8
SevaStopAll <sevastopallrh@yandex.ru>
2023-03-10 08:02:11 +0400

    1. Генератор HTML [#504838 #398980]

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -1,9 +1,9 @@
-package src.main.java.ru.job4j.dreamjob.controller;
+package ru.job4j.dreamjob.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/index")
