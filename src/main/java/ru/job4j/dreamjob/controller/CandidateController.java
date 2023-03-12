c46c15130479a7a6315c84dadf7bcf87f7bfeeec
SevaStopAll <Sevastopallrh@yandex.ru>
2023-03-12 11:54:15 +0400

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -1,17 +1,15 @@
 package ru.job4j.dreamjob.controller;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.Candidate;
-import ru.job4j.dreamjob.model.Vacancy;
-import ru.job4j.dreamjob.repository.MemoryCandidateRepository;
-import ru.job4j.dreamjob.repository.CandidateRepository;
 import ru.job4j.dreamjob.service.CandidateService;
-import ru.job4j.dreamjob.service.SimpleCandidateService;
 
 @Controller
 @RequestMapping("/candidates")
+@ThreadSafe
 public class CandidateController {
 
         private final CandidateService candidateService;
--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -1,19 +1,15 @@
 package ru.job4j.dreamjob.controller;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.dreamjob.model.Vacancy;
-import ru.job4j.dreamjob.repository.MemoryVacancyRepository;
-import ru.job4j.dreamjob.repository.VacancyRepository;
-import ru.job4j.dreamjob.service.SimpleVacancyService;
 import ru.job4j.dreamjob.service.VacancyService;
-
-import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/vacancies")
+@ThreadSafe
 public class VacancyController {
 
     private final VacancyService vacancyService;
