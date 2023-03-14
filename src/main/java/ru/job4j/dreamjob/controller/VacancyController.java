30ebc33f5163caa8103919ef01358fc5a26ac91d
SevaStopAll <sevastopallrh@yandex.ru>
2023-03-14 13:25:08 +0400

    2. Тестируем VacancyController [#504867]

--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -6,13 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.job4j.dreamjob.dto.FileDto;
-import ru.job4j.dreamjob.model.User;
 import ru.job4j.dreamjob.model.Vacancy;
 import ru.job4j.dreamjob.service.CityService;
 import ru.job4j.dreamjob.service.FileService;
 import ru.job4j.dreamjob.service.VacancyService;
-
-import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/vacancies")
@@ -30,27 +27,10 @@ public class VacancyController {
     }
 
     @GetMapping
-    public String getAll(Model model, HttpSession session) {
-        var user = (User) session.getAttribute("user");
-        if (user == null) {
-            user = new User();
-            user.setName("Гость");
-        }
+    public String getAll(Model model) {
         model.addAttribute("vacancies", vacancyService.findAll());
-        model.addAttribute("user", user);
         return "vacancies/list";
     }
-
-    /*@GetMapping({"/", "/index"})
-    public String getIndex(Model model, HttpSession session) {
-        var user = (User) session.getAttribute("user");
-        if (user == null) {
-            user = new User();
-            user.setName("Гость");
-        }
-        model.addAttribute("user", user);
-        return "index";
-    }*/
 
     @GetMapping("/create")
     public String getCreationPage(Model model) {
